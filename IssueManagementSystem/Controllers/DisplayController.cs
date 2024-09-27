f61623de20d1ee399857600b3a9a802605feba4f
Hirushan.N <Hirushan.N@HIRUSHAN>
2024-09-27 14:32:29 +0530

    develop statics cards for each lines to display issue counts

--- IssueManagementSystem/Controllers/DisplayController.cs
+++ IssueManagementSystem/Controllers/DisplayController.cs
@@ -1,126 +1,121 @@
 ﻿using IssueManagementSystem.Models;
 using System;
-using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
 
 namespace IssueManagementSystem.Controllers
 {
     public class DisplayController : Controller
     {
-       
-      
-        public ActionResult slect()
+        // GET: Index
+        public ActionResult Index()
         {
-            try
+            using (var db = new issue_management_systemEntities1())
             {
+                // Check department in session and set counts
+                SetViewBagCounts(db);
                 return View();
             }
-            catch (Exception ex)
-            {
-                return View("Error"); // Return an error view
-            }
         }
 
- 
-        public ActionResult Rasp(int id)
-
+        private void SetViewBagCounts(issue_management_systemEntities1 db)
         {
-            try
+            // Depending on the department, count issues
+            if ((string)Session["department"] == "MD")
             {
-                using (issue_management_systemEntities1 db = new issue_management_systemEntities1())
-                {
-
-
-                    ViewBag.id = id;
-                    //   ViewBag.issueoccourInfo = db.issue_occurrence.Where(x => x.line_line_id == id && x.issue_satus=="1").Count();
-                    //if (ViewBag.issueoccourInfo == 0 && id == 3)
-                    //{
-                    //    return Redirect("http://192.168.1.30:84/Report/GagingD7/" + id);
-                    //}
-                    //else if (ViewBag.issueoccourInfo == 0 && id == 17)
-                    //{
-                    //    return Redirect("http://192.168.1.30:84/Report/GagingD8/" + id);
-                    //}
-                    //else
-                    return View();
-
-                }
+                ViewBag.BrakedownCount = db.issue_occurrence.Count(x => x.issue_issue_ID == 1 && x.issue_satus == "1");
+                ViewBag.ITIsuue = db.issue_occurrence.Count(x => x.issue_issue_ID == 2 && x.issue_satus == "1");
+                ViewBag.TechnicalIssue = db.issue_occurrence.Count(x => x.issue_issue_ID == 3 && x.issue_satus == "1");
+                ViewBag.MaterialDelayCount = db.issue_occurrence.Count(x => x.issue_issue_ID == 4 && x.issue_satus == "1");
+                ViewBag.QualityIsuue = db.issue_occurrence.Count(x => x.issue_issue_ID == 5 && x.issue_satus == "1");
             }
-            catch (Exception ex)
+            else
             {
-                return View("Error");
+                string location = Session["location"].ToString();
+                // Similar logic, but can add filtering by location if required
+                ViewBag.BrakedownCount = db.issue_occurrence.Count(x => x.location == location && x.issue_issue_ID == 1 && x.issue_satus == "1");
+                ViewBag.ITIsuue = db.issue_occurrence.Count(x => x.location == location && x.issue_issue_ID == 2 && x.issue_satus == "1");
+                ViewBag.TechnicalIssue = db.issue_occurrence.Count(x => x.location == location && x.issue_issue_ID == 3 && x.issue_satus == "1");
+                ViewBag.MaterialDelayCount = db.issue_occurrence.Count(x => x.location == location && x.issue_issue_ID == 4 && x.issue_satus == "1");
+                ViewBag.QualityIsuue = db.issue_occurrence.Count(x => x.location == location && x.issue_issue_ID == 5 && x.issue_satus == "1");
             }
-
         }
+
+        public ActionResult Rasp(int id)
+        {
+            using (var db = new issue_management_systemEntities1())
+            {
+                // Retrieve issue counts for a specific line ID
+                var lineCounts = new
+                {
+                    BrakedownCount = db.issue_occurrence.Count(x => x.line_line_id == id && x.issue_issue_ID == 1 && x.issue_satus == "1"),
+                    ITIsuue = db.issue_occurrence.Count(x => x.line_line_id == id && x.issue_issue_ID == 2 && x.issue_satus == "1"),
+                    TechnicalIssue = db.issue_occurrence.Count(x => x.line_line_id == id && x.issue_issue_ID == 3 && x.issue_satus == "1"),
+                    MaterialDelayCount = db.issue_occurrence.Count(x => x.line_line_id == id && x.issue_issue_ID == 4 && x.issue_satus == "1"),
+                    QualityIsuue = db.issue_occurrence.Count(x => x.line_line_id == id && x.issue_issue_ID == 5 && x.issue_satus == "1")
+                };
+
+                ViewBag.BrakedownCount = lineCounts.BrakedownCount;
+                ViewBag.ITIsuue = lineCounts.ITIsuue;
+                ViewBag.TechnicalIssue = lineCounts.TechnicalIssue;
+                ViewBag.MaterialDelayCount = lineCounts.MaterialDelayCount;
+                ViewBag.QualityIsuue = lineCounts.QualityIsuue;
+                ViewBag.id = id; // Pass the line ID to the View
+
+                return View();
+            }
+        }
+
+        public JsonResult FilterSelectBoxes()
+        {
+            string query = @"SELECT DISTINCT deps.department_id, deps.department_name,
+                                    lines.line_id, lines.line_name, l_map.issues AS issues
+                             FROM issue_management_system.dbo.departments deps
+                             JOIN issue_management_system.dbo.lines lines ON lines.department_id = deps.department_id
+                             JOIN issue_management_system.dbo.line_map l_map ON l_map.line_id = lines.line_id";
+            using (var db = new issue_management_systemEntities1())
+            {
+                var result = db.Database.SqlQuery<TempClasses.tempClass10>(query).ToList();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
         public ActionResult Stores(string location)
         {
-            try
-            {
-                ViewBag.location = location;
-                return View();
-            }
-            catch(Exception e) {
-                return View("Error");
-            }
-
+            ViewBag.location = location;
+            return View();
         }
+
         public ActionResult machineshop(int id)
         {
-            try { 
             ViewBag.id = id;
             return View();
-            }
-            catch (Exception e)
-            {
-                return View("Error");
-            }
         }
 
         public ActionResult accessoriesHeatTreatment(int id)
         {
-            try
-            {
-                ViewBag.id = id;
-                return View();
-            }
-            catch (Exception e)
-            {
-                return View("Error");
-            }
+            ViewBag.id = id;
+            return View();
         }
+
         public ActionResult Maintenance(string location)
-
         {
-            try
-            {
-                ViewBag.location = location;
-                return View();
-            }
-            catch (Exception e)
-            {
-                return View("Error");
-            }
-
+            ViewBag.location = location;
+            return View();
         }
+
         public ActionResult test()
         {
-            try
-            {
-                return View();
-            }catch(Exception e)
-            {
-                return View("Error");
-            }
-
+            return View();
         }
-        [HttpPost]//solvedIssueMethod
+
+        [HttpPost]
         public JsonResult updateScreen()
         {
             try
             {
+                // Logic to update the screen if needed
                 return Json(true);
             }
             catch (Exception ex)
@@ -128,25 +123,26 @@ namespace IssueManagementSystem.Controllers
                 return Json(new { success = false, message = "An error occurred while updating the screen." });
             }
         }
-        class machines_temp{
+
+        class machines_temp
+        {
             public string machine_machine_id { get; set; }
         }
 
         [HttpPost]
-        public ActionResult getBlinkingMachinesList(string line)
+        public JsonResult getBlinkingMachinesList(string line)
         {
             try
             {
                 int lineID = Int32.Parse(line);
 
-                using (issue_management_systemEntities1 db = new issue_management_systemEntities1())
+                using (var db = new issue_management_systemEntities1())
                 {
-                    //List<issue_occurrence> c = db.issue_occurrence.Where(x=>x.line_line_id== lineID && x.issue_issue_ID == 1).ToList();
-                    //foreach (issue_occurrence x in c) { System.Diagnostics.Debug.WriteLine(x.machine_machine_id); }
-                    String query_1 = "SELECT issue_occurrence.machine_machine_id FROM issue_occurrence WHERE issue_occurrence.line_line_id =" + line + " AND issue_occurrence.issue_issue_ID=1 AND issue_occurrence.issue_satus=1";
-                    var c = db.Database.SqlQuery<machines_temp>(query_1).ToList();
+                    // Fetch machines with active issues for the specified line
+                    string query = "SELECT issue_occurrence.machine_machine_id FROM issue_occurrence WHERE issue_occurrence.line_line_id = @lineID AND issue_occurrence.issue_issue_ID = 1 AND issue_occurrence.issue_satus = 1";
+                    var machines = db.Database.SqlQuery<machines_temp>(query, new SqlParameter("@lineID", lineID)).ToList();
 
-                    return Json(c, JsonRequestBehavior.AllowGet);
+                    return Json(machines, JsonRequestBehavior.AllowGet);
                 }
             }
             catch (Exception ex)
