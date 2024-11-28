b91fc8d5c416aa4192a26d12ae3f3a4f6be5d211
Hirushan.N <Hirushan.N@HIRUSHAN>
2024-11-28 17:38:25 +0530

    new

---
+++ IssueManagementSystem/Migrations/202411260427536_ims2.cs
@@ -0,0 +1,16 @@
+﻿namespace IssueManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ims2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
